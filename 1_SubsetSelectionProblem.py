cfd571423da904ebd21f0c4b677d510a381d840e
MONISHKA MITTAL <148357574+MONISHKA1607@users.noreply.github.com>
2025-06-11 01:58:39 +0530

    Add files via upload

---
+++ 1_SubsetSelectionProblem.py
@@ -0,0 +1,26 @@
+# 1. Problem Statement:
+# Find all the subsets from a set of numbers whose sum is zero.
+
+# Constraint: Subset size must be 5
+# Set={-12, -3, -6, 7, 2, -2, 6, 3, 9, -7, -5, -8, 1, 11, -9, -4}
+
+import random as r
+Set         = set([-12, -3, -6, 7, 2, -2, 6, 3, 9, -7, -5, -8, 1, 11, -9, -4])
+SetSize     = 5
+ResultList  = set()
+Iterations  = 1000 
+
+for i in range(Iterations):
+    
+    Chromosome = r.sample(list(Set), SetSize)
+    Chromosome.sort()
+    
+    if sum(Chromosome) == 0:
+        ResultList.add(tuple(Chromosome))
+
+
+for r in ResultList:
+	print (r)
+
+
+print ("\nTotal Sets: ", len(ResultList))
